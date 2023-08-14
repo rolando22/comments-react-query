0a44192be810df58270290cfb5b550f952c71892
rolando22 <rolando2207@gmail.com>
2023-08-13 23:33:09 -0300

    [ADD] maquetación de la página principal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,15 @@ import './App.css';
 
 export function App() {
     return (
-        <></>
+        <main className='grid h-screen grid-cols-2'>
+            <section className='col-span-1 p-8 bg-white'></section>
+            <section className='col-span-1 p-8 bg-black'>
+                <form className='block max-w-xl px-4 m-auto'>
+                    <button className='mt-4 px-12 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm py-2.5 text-center mr-2 mb-2'>
+                        Enviar comentario
+                    </button>
+                </form>
+            </section>
+        </main>
     );
 }
